2555b3c415a0fb51aa432bf2e6f80635f47c47ab
emilykidd3 <e.kidd61@yahoo.com>
2021-05-26 21:51:28 -0700

    updated nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -10,13 +10,24 @@ import {
     DropdownToggle,
     DropdownMenu,
     DropdownItem,
+    Collapse,
 } from "reactstrap"
 
 
 function Navigation() {
     return(
         <div>
-            NAVBAR
+            <Navbar>
+                <NavbarToggler onClick={toggleNavbar} />
+                <Collapse isOpen={!collapsed} navbar>
+                    <Nav navbar>
+                        <NavItem>
+                            Home
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+            HABIT TRACKER
         </div>
     )
 }
