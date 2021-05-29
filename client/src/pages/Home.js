bd8a5ec8df59f65df97b19c8959fa1b43a9ee1ec
emilykidd3 <e.kidd61@yahoo.com>
2021-05-28 22:12:41 -0700

    fixed key typo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import moment from 'moment'
+import React from "react";
+import moment from "moment";
 
 const Home = () => {
-    const habit = {name: 'Gym', days: ["2021-05-25", "2021-05-26", "2021-05-27"]}
+  const habit = {
+    name: "Gym",
+    days: ["2021-05-25", "2021-05-26", "2021-05-27", "2021-05-28"],
+  };
 
-    let daysRecorded = habit.days;
-    let dayGrids = [];
-    for (let i = 0; i < 365; i++) {
-        let dayGridDate = moment().subtract(i, 'days').format("YYYY-MM-D");
-        if (dayGridDate.toString() === daysRecorded[daysRecorded.length - 1]) {
-            dayGrids.unshift(<div className="day day--active" kay={i} />);
-        } else {
-            dayGrids.unshift(<div className="day" kay={i} />);
-        }
+  let daysRecorded = habit.days;
+  let dayGrids = [];
+  for (let i = 0; i < 365; i++) {
+    let dayGridDate = moment().subtract(i, "days").format("YYYY-MM-D");
+    if (dayGridDate.toString() === daysRecorded[daysRecorded.length - 1]) {
+      dayGrids.unshift(<div className="day day--active" key={i} />);
+    } else {
+      dayGrids.unshift(<div className="day" key={i} />);
     }
-    return(
-        <div>
-            <div style={{textAlign: "center"}}>
-                <h4 >{habit.name}</h4>
-            </div>
-            <div className="days">{dayGrids}</div>
-        </div>
-    )
-}
+  }
+  return (
+    <div>
+      <div style={{ textAlign: "center" }}>
+        <h4>{habit.name}</h4>
+      </div>
+      <div className="days">{dayGrids}</div>
+    </div>
+  );
+};
 
 export default Home;
