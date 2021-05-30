0b57e41887074051ea32e4ad8f8609689c6cd3b3
emilykidd3 <e.kidd61@yahoo.com>
2021-05-29 23:36:05 -0700

    made links to home and add habits pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 import Home from './pages/Home'
+import AddHabit from './pages/AddHabit'
 
 import Nav from './components/Nav'
 
@@ -13,6 +14,8 @@ function App() {
         <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/addhabit" component={AddHabit} />
         </Switch>
       </div>
     </Router>
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,51 +1,46 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
-    Navbar,
-    Nav,
-    NavItem,
-    NavLink,
-    NavbarToggler,
-    Collapse,
-    Container,
-    Row,
-    Col
-} from "reactstrap"
-
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler,
+  Collapse,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
 
 function Navigation() {
-    const [collapsed, setCollapsed] = useState(true);
+  const [collapsed, setCollapsed] = useState(true);
 
-    const toggleNavbar = () => setCollapsed(!collapsed);
+  const toggleNavbar = () => setCollapsed(!collapsed);
 
-    return(
-        <Container style={{marginLeft: "0", marginRight: "0", display: "flex"}}>
-            <Row>
-                <Col xs="1">
-                    <Navbar style={{color: "black"}} light>
-                        <NavbarToggler onClick={toggleNavbar} />
-                        <Collapse isOpen={!collapsed} navbar>
-                            <Nav navbar>
-                                <NavItem>
-                                    <NavLink>
-                                        Home
-                                    </NavLink>
-                                    <NavLink to="/newhait">
-                                        Add a Habit
-                                    </NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </Navbar>
-                </Col>
-                <Col xs="11" style={{display: "flex"}}>
-                    <h1 style={{margin: "auto 30px"}}>
-                        HABIT TRACKER
-                    </h1>
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container style={{ marginLeft: "0", marginRight: "0", display: "flex" }}>
+      <Row>
+        <Col xs="1">
+          <Navbar style={{ color: "black" }} light>
+            <NavbarToggler onClick={toggleNavbar} />
+            <Collapse isOpen={!collapsed} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink href="/home">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/addhabit">Add a Habit</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </Col>
+        <Col xs="11" style={{ display: "flex" }}>
+          <h1 style={{ margin: "auto 30px" }}>HABIT TRACKER</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Navigation;
