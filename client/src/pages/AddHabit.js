9a1d77fff486bd2e0fa1ae0964ffd79244b1ab4d
emilykidd3 <e.kidd61@yahoo.com>
2021-05-30 14:29:47 -0700

    added stying to add habit page

--- client/src/pages/AddHabit.js
+++ client/src/pages/AddHabit.js
@@ -11,13 +11,13 @@ import {
 
 const AddHabit = () => {
     return(
-        <Form style={{margin: "50px 0 0 50px"}}> 
+        <Form style={{margin: "50px 0 0 150px"}}> 
             <FormGroup row>
                 <Label for="newHabit" sm={2}>Habit Name</Label>
                 <Col sm={6}>
                     <Input type="text" name="habit" id="habitName"/>
                 </Col>
-                <Button sm={2}>Add</Button>
+                <Button sm={2} style={{width: "90px", marginLeft: "50px"}}>Add</Button>
             </FormGroup>
         </Form>
     )
