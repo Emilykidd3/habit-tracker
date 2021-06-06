2a2bacd55cfdd206507de15795d1f1a7134ae34e
emilykidd3 <e.kidd61@yahoo.com>
2021-06-06 09:06:53 -0700

    added user model

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const bycrypt = require('bycrpt');
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bycrypt.hash(this.password, saltRounds);
+
+        const User = mongoose.model('User', userSchema);
+
+        module.exports = User;
+    }
+})
